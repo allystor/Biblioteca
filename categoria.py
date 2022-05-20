22888ad4b450814a5b414d487d6a411a5e3d66ac
allystor <alvarojoao17@gmail.com>
2022-05-20 00:32:36 -0300

    tentando criar alguma coisa daora

--- categoria.py
+++ categoria.py
@@ -29,7 +29,78 @@ class Categoria():
     #=================================================#
 
     def incluirCategoria(listaCategorias):
-        pass
+        nome = input("Digite o nome da categoria: ")
+        descricao = input("Digite a descricao da categoria: ")
+        assunto = input("Digite o assunto da categoria: ")
+        categoria = Categoria(nome, descricao, assunto)
+        listaCategorias.append(categoria)
+    
+    def excluirCategoria(listaCategorias):
+        nome = input("Digite o nome da categoria: ")
+        for categoria in listaCategorias:
+            if categoria.get_nome() == nome:
+                listaCategorias.remove(categoria)
+                print("Categoria excluida com sucesso!")
+                break
+            
+    def listarCategorias(listaCategorias):
+        for categoria in listaCategorias:
+            print(f"{categoria.get_nome()}")
+            
+    def alterarCategoria(listaCategorias):
+        print("Alterando categorias")
+        
+        busca = str(input("Informe o titulo do livro que deseja alterar: "))
+        alterar = []
+        contador = 0
+
+        for categoria in listaCategorias:
+            if busca.lower() == categoria.get_nome().lower():
+                contador += 1
+                alterar.append(categoria)
+
+        if contador == 0:
+            print("Não foi encontrado nenhuma categoria com esse nome!")
+
+        elif contador == 1:
+            categoria = alterar[0]
+
+        elif contador > 1:
+            print("Foram encontrados mais de uma categoria com esse nome!")
+            contador = 1
+
+            for i in listaCategorias:
+                print(f"{contador} - {i.get_nome()}")
+                print(f"{contador} - {i.get_descricao()}")
+                print(f"{contador} - {i.get_assunto()}")
+                contador += 1
+
+            opcao = int(input("Qual desses livros deseja alterar?: "))
+            categoria = alterar[opcao - 1]
+
+        while True:
+
+            print(f"Alterando livro '{categoria.get_nome()}'")
+            print("Opções disponíveis: para alterar:\n1 - Nome\n2 - Descrição\n3 - Assunto")
+            print("Digite 'sair' para sair")
+            escolha = input("Oque deseja alterar na categoria? ")
+
+            if opcao == "sair":
+                break
+
+            elif opcao == "nome":
+                categoria.set_nome(input("Digite o novo titulo: "))
+            
+            elif escolha == "descricao":
+                categoria.set_descricao(input("Digite o novo autor: "))
+
+            elif escolha == "assunto":
+                categoria.set_assunto(input("Digite o novo assunto: "))
+
+            else:
+                print("Escolha invalida...")
+                
+    #ta rentável?
 
 listaCategorias = [
     Categoria("Biografia", "Gênero no qual o autor narra sobre a vida de uma pessoa ou de várias pessoas.", None),
